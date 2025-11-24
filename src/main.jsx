5b98ffe548a4783842123972adbe130d6535e971
CBreezyJR <kazzyjr123@gmail.com>
2025-11-24 11:42:27 +0300

    flowbite

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "flowbite/dist/flowbite.css";
+import "flowbite/dist/flowbite.min.css";
 import "./styles/design-system.css";
 import "./index.css";
 
